ffcc012b7aef5da37965880cb98d5ebf250fd946
PoroR <dmytrivroma@gmail.com>
2017-07-08 20:11:25 +0300

    qq

--- index.js
+++ index.js
@@ -1,35 +1,62 @@
-// var A = document.getElementById("messageText").value;
-var noneBill = document.getElementById("getBillMessage");
-var noneBoob = document.getElementById("getBoobMessage");
+// var noneBill = document.getElementById("getBillMessage");
+// var noneBoob = document.getElementById("getBoobMessage");
 
-var obj = {}
-function sendBill(obj){
-   this.obj.name = "Bill";
-   this.obj.val = document.getElementById("messageText").value;
-   this.obj.toString = function(){return "User " + this.name + " get message: \n" + this.val};
+var massBill = [];
+var massBoob = [];
+var massCustom = [];
+var objBill = {};
+var objBoob = {};
+var objCustom = {};
+function sendBill(objBill, massBill){
+   this.objBill.name = "Bill";
+   this.objBill.val = document.getElementById("messageText").value;
+   this.objBill.toString = function(){return "User " + this.name + " get message: \n" + this.val};
+   Object.freeze(this.objBill);
+   this.massBill.push(this.objBill);
+   document.getElementById("getBillMessage").value = this.massBill.join("\n" + "\n");
 };
-function sendBoob(obj){
-   this.obj.name = "Boob";
-   this.obj.val = document.getElementById("messageText").value;
-   this.obj.toString = function(){return "User " + this.name + " get message: \n" + this.val};
+function sendBoob(objBoob, massBoob){
+   this.objBoob.name = "Boob";
+   this.objBoob.val = document.getElementById("messageText").value;
+   this.objBoob.toString = function(){return "User " + this.name + " get message: \n" + this.val};
+   this.massBoob.push(this.objBoob);
+   document.getElementById("getBoobMessage").value = this.massBoob.join("\n" + "\n");
+};
+function sendCustom(objCustom, massCustom){
+   this.objCustom.name = document.getElementById("yourName").value;
+   this.objCustom.val = document.getElementById("yourText").value;
+   this.objCustom.toString = function(){return "User " + this.name + " get message: \n" + this.val};
+   this.massCustom.push(this.objCustom);
+   document.getElementById("getCustomMessage").value = this.massCustom.join("\n" + "\n");
 };
 function showBill(){
-   document.getElementById("getBillMessage").value = obj;
-      var noneBill = document.getElementById("getBillMessage");
-      if(noneBill.style.display != "block"){
-         noneBill.style.display = "block"
-      }
-      else {
-         noneBill.style.display = "none"
-      }
-   };
+   var noneBill = document.getElementById("getBillMessage");
+   if(noneBill.style.display != "block"){
+      noneBill.style.display = "block";
+   }
+   else noneBill.style.display = "none";
+
+}
 function showBoob(){
-   document.getElementById("getBoobMessage").value = obj;
-      var noneBoob = document.getElementById("getBoobMessage");
-      if(noneBoob.style.display != "block"){
-         noneBoob.style.display = "block"
-      }
-      else {
-         noneBoob.style.display = "none";
-      }
+   var noneBoob = document.getElementById("getBoobMessage");
+   if(noneBoob.style.display != "block"){
+      noneBoob.style.display = "block";
+   }
+   else noneBoob.style.display = "none";
    };
+function showCustom(){
+   var noneCustom = document.getElementById("getCustomMessage");
+   if(noneCustom.style.display != "block"){
+         noneCustom.style.display = "block";
+   }
+   else noneCustom.style.display = "none";
+   };
+function showBill1(){
+   document.getElementById("getAllMessage").value = this.massBill.join("\n" + "\n");
+}
+function showBoob1(){
+   document.getElementById("getAllMessage").value = this.massBoob.join("\n" + "\n");
+}
+function showCustom1(){
+   document.getElementById("getAllMessage").value = this.massCustom.join("\n" + "\n");
+}
